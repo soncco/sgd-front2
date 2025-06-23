5843039f508b71f84033470fe8247ae2642cf949
Gustavo Ochoa <workgustavo1503@gmail.com>
2025-06-22 21:53:52 -0500

    feat/añadir paginas de listado, creacion y edicion de personas (pruebas)

---
+++ src/pages/sistema/PersonaForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <DynamicPage :titulo="titulo" :form="form" />
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import DynamicPage from 'src/components/DynamicPage.vue'
+
+const route = useRoute()
+const id = route.params.id
+
+const isEdit = !!id
+
+const fields = [
+    { field: 'email', label: 'Email', type: 'email' },
+    { field: 'nombres', label: 'Nombres', type: 'text' },
+    { field: 'apellidos', label: 'Apellidos', type: 'text' },
+    { field: 'empresa', label: 'Empresa', type: 'text' },
+    { field: 'celular', label: 'Celular', type: 'tel' },
+    { field: 'tipo_persona', label: 'Tipo de persona', type: 'apiselect' },
+    { field: 'tipo_doc', label: 'Tipo de documento', type: 'apiselect' },
+    { field: 'documento', label: 'Documento', type: 'text' },
+    //{ field: 'oficina', label: 'Oficina', type: 'apiselect' },
+    //{ field: 'password', label: 'Contraseña', type: 'password' },
+    //{ field: 'password_verification', label: 'Contraseña', type: 'password' },
+]
+
+const titulo = {
+  title: isEdit ? 'Editar Persona' : 'Nueva Persona',
+  icon: 'edit',
+  buttons: [
+    { label: 'Ver todas las Personas', icon: 'list', route: '/personas' },
+  ],
+}
+
+const form = {
+    endpoint: isEdit ? `/api/base/personas/${id}/` : '/api/base/personas/',
+    fields,
+    urlLista: '/personas',
+    isEdit,
+}
+</script>
